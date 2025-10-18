7be263fcd1d84be646a9a036e3d5a73958df2a06
Zizhen Ji <zizhen@mac.home>
2025-10-18 13:02:06 -0400

    day1

--- day1/task.py
+++ day1/task.py
@@ -1,1 +1,8 @@
-print("hello world!!!")
+#print("hello world\n \bhello world")
+#print("Hello"+" xuan")
+#print("Hello"+" " + input("What is your name?\n") + "!")
+
+#name = input("What is your name?\n")
+username = input("What is your name?")
+length = len(username)
+print(length)
